e303c05010c7bfbb9b9962b6585ae9d75545cdbc
Rawlin Peters <rbpeters@rams.colostate.edu>
2014-04-25 12:38:46 -0600

    Added initial functionality to federation.php

--- federation.php
+++ federation.php
@@ -4,34 +4,41 @@
    include 'nav.php';
 ?>
 
+<?php if ($_SESSION['username'] != 'guest') {?>
 <script type="text/javascript">
 
+<?php 
+for ($i = 0; $i < 3; $i++) {
+?>
+
 	$(document).ready(function() {
-		$(".confed").hover(function() {
-			$(".hidden").show();
+
+		$(".confed#link<?php echo $i; ?>").hover(function() {
+
+			//include AJAX call here to fill the description
+			
+			$(".hidden#desc<?php echo $i; ?>").show();
+		},
+		function() {
+			$(".hidden#desc<?php echo $i; ?>").hide();
 		});
 	});
 
+<?php } ?>
+
 </script>
+<?php } ?>
 
 <div class="wrapper">
 	<div class="left"> 
 		<h3>The Federation:</h3>
 		<ul>
-			<li class="confed">Site 1</li>
-			<div class="hidden">Description</div>
-			<li class="confed">Site 2</li>
-			<div class="hidden">Description</div>
-			<li class="confed">Site 3</li>
-			<div class="hidden">Description</div>
-			<li class="confed">Site 4</li>
-			<div class="hidden">Description</div>
-			<li class="confed">Site 5</li>
-			<div class="hidden">Description</div>
-			<li class="confed">Site 6</li>
-			<div class="hidden">Description</div>
-			<li class="confed">Site 7</li>
-			<div class="hidden">Description</div>
+			<li id="link0" class="confed">Site 1</li>
+			<div id="desc0" class="hidden"><p>Description</p></div>
+			<li id="link1" class="confed">Site 2</li>
+			<div id="desc1" class="hidden"><p>Description</p></div>
+			<li id="link2" class="confed">Site 3</li>
+			<div id="desc2" class="hidden"><p>Description</p></div>
 		</ul>
 	</div>
 	
